52aecdde654b353a586635bc620a8bec92650239
George Thayamkery <george.thayamkery@gmail.com>
2021-10-18 17:18:53 -0500

    publish winners

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,8 +16,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/start">
-            Getting Started
+            to="/docs/winners">
+            See the winners
           </Link>
         </div>
       </div>
